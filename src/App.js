e247b81f57d32e7cc9e111e860aec571618037f0
Fran Linde Blazquez <franlindebl@gmail.com>
2023-03-09 15:57:50 +0100

    Added simple language selector

--- src/App.js
+++ src/App.js
@@ -1,11 +1,41 @@
 import './App.scss';
 import RegisterForm from './components/RegisterForm/RegisterForm';
+import React from 'react';
+import { IntlProvider } from 'react-intl';
+import Spanish from './lang/es.json';
+import French from './lang/fr.json';
+import English from './lang/en.json';
+
+const locale = navigator.language;
+let defaultMessages = Spanish;
+switch (locale) {
+  case 'fr-FR':
+    defaultMessages = French;
+    break;
+  case 'en-EN':
+    defaultMessages = English;
+    break;
+  case 'es-ES':
+    defaultMessages = Spanish;
+    break;
+  default:
+    defaultMessages = English;
+}
 
 function App() {
+  const [messages, setMessages] = React.useState(defaultMessages);
+
   return (
-    <div className='App'>
-      <RegisterForm></RegisterForm>
-    </div>
+    <IntlProvider locale={locale} messages={messages}>
+      <div className='App'>
+        <RegisterForm></RegisterForm>
+      </div>
+
+      <h2>Selector de idioma</h2>
+      <button onClick={() => setMessages(Spanish)}>Spanish</button>
+      <button onClick={() => setMessages(English)}>English</button>
+      <button onClick={() => setMessages(French)}>French</button>
+    </IntlProvider>
   );
 }
 
--- src/index.js
+++ src/index.js
@@ -3,33 +3,11 @@ import ReactDOM from 'react-dom/client';
 import './index.scss';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import { IntlProvider } from 'react-intl';
-import Spanish from './lang/es.json';
-import French from './lang/fr.json';
-import English from './lang/en.json';
-
-const locale = navigator.language;
-let messages = Spanish;
-switch (locale) {
-  case 'fr-FR':
-    messages = French;
-    break;
-  case 'en-EN':
-    messages = English;
-    break;
-  case 'es-ES':
-    messages = Spanish;
-    break;
-  default:
-    messages = English
-}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <IntlProvider locale={locale} messages={messages}>
-      <App />
-    </IntlProvider>
+    <App />
   </React.StrictMode>
 );
 
